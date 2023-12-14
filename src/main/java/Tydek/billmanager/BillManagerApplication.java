c3c0c0e82cedbdb01b20c1def37024a1c6adc6a7
Ginestra <ginestraalex1@gmail.com>
2023-12-14 16:17:44 +0100

    First commit

---
+++ src/main/java/Tydek/billmanager/BillManagerApplication.java
@@ -0,0 +1,13 @@
+package Tydek.billmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tydek/billmanager/BillManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package Tydek.billmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
