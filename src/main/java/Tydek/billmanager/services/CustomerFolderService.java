9d7eb6699d7267703b8e366c794731b7c9926ab2
Ginestra <ginestraalex1@gmail.com>
2023-12-17 21:08:55 +0100

    Service : CustomerFolderService

---
+++ src/main/java/Tydek/billmanager/services/CustomerFolderService.java
@@ -0,0 +1,29 @@
+package Tydek.billmanager.services;
+
+import Tydek.billmanager.domains.customerFofolder.CustomerFolder;
+import Tydek.billmanager.repositories.CustomerFolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerFolderService {
+
+    @Autowired
+    private CustomerFolderRepository customerFolderRepository;
+
+    public CustomerFolder createCustomerFolder() {
+        return this.customerFolderRepository.save(new CustomerFolder());
+    }
+
+    public List<CustomerFolder> getAllCustomerFolders() {
+        return this.customerFolderRepository.findAll();
+    }
+
+    public CustomerFolder getCustomerFolder(long id) {
+        Optional<CustomerFolder> res = this.customerFolderRepository.findById(id);
+        return res.isEmpty() ? null : res.get();
+    }
+}
